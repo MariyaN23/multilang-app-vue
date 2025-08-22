4f4cbd1efd3f8a81bf9530f7bb111a2d66fa9b79
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-08-22 12:19:56 +0300

    localization

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { computed } from "vue";
+
+const {t, locale, availableLocales } = useI18n({
+  locale: 'en',
+  messages: {
+    en: {
+      msg: 'hello',
+      named: '{msg} world!',
+      list: '{0} to you!',
+      literal: "{'goodbye'} world!",
+      the_world: 'the world',
+      dio: 'DIO:',
+      linked: '@:dio @:the_world !!!!',
+      banana: 'no bananas | {n} banana | {n} bananas',
+    },
+    ja: {
+      msg: 'こんにちは',
+      named: '{msg} 世界！',
+      list: '{0} 世界！',
+      literal: "{'こんにちは'} 世界！",
+      the_world: 'ザ・ワールド！',
+      dio: 'ディオ:',
+      linked: '@:dio @:the_world ！！！！',
+      banana: 'バナナ',
+    },
+  }
+})
+
+const msg = computed(() => t('msg'))
+</script>
+
+<template>
+  <div>
+    <select v-model="locale">
+      <option v-for="loc in availableLocales" :key="`locale-${loc}`" :value="loc">{{ loc }}</option>
+    </select>
+  </div>
+  <div>
+    <p>{{ t('named', {msg}) }}</p>
+    <p>{{ t('list', [msg]) }}</p>
+    <p>{{ t('literal') }}</p>
+    <p>{{ t('linked') }}</p>
+  </div>
+  <div>
+    <p>{{ t('banana', 1, { named: { n: 1 } }) }}</p>
+    <p>{{ t('banana', 1) }}</p>
+    <p>{{ t('banana', 100, { named: { n: 'too many' } }) }}</p>
+  </div>
+</template>
+
+<style scoped></style>
